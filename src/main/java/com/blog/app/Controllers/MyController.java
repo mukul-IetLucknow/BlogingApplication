8e899b63603d40ad14e69c4e658f5a44facc5d79
Mukul <sharmamukul437@gmail.com>
2022-09-12 22:56:58 +0530

    changed done

--- src/main/java/com/blog/app/Controllers/MyController.java
+++ src/main/java/com/blog/app/Controllers/MyController.java
@@ -3,6 +3,6 @@ package com.blog.app.Controllers;
 public class MyController {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("hi mukul today is 12 sep");
 	}
 }
